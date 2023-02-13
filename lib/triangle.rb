1c3377024b62158cba107380db3f05ce108b3f3c
evance-14 <kiropevans097@gmail.com>
2023-02-14 00:07:09 +0300

    completed assignment

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,27 @@
 class Triangle
-  # write code here
+  attr_reader :a, :b, :c
+
+  def initialize(a, b, c)
+    @a, @b, @c = a, b, c
+  end
+
+  def kind
+    validate_triangle
+    if a == b && b == c
+      :equilateral
+    elsif a == b || b == c || a == c
+      :isosceles
+    else
+      :scalene
+    end
+  end
+
+  private
+
+  def validate_triangle
+    sides = [a, b, c].sort
+    raise TriangleError if sides[0] <= 0 || sides[0] + sides[1] <= sides[2]
+  end
+
+  class TriangleError < StandardError; end
 end
